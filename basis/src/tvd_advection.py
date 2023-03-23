b751393e6e23e369c8f7d9e10994d414e3bc9f8a
Ethan Pierce <ethan.pierce@colorado.edu>
2023-03-23 11:09:15 -0600

    Sketch TVD advection algorithm

--- basis/src/tvd_advection.py
+++ basis/src/tvd_advection.py
@@ -15,13 +15,150 @@ from landlab import RasterModelGrid
 class AdvectTVD:
     """Implement the TVD advection scheme."""
 
-    def __init__(self, grid, field: str):
+    def __init__(self, grid, field: str, velocity: str):
         """Initialize the advector with a Landlab grid and indicate the field to advect."""
         self._grid = grid
+        self.time_elapsed = 0.0
 
         if field in self._grid.at_node.keys():
             self._field = field
         else:
             raise ValueError("Input field " + str(field) + " not found at grid nodes.")
 
-    
+        if velocity in self._grid.at_link.keys():
+            self._vel = velocity
+        else:
+            raise ValueError("Velocity field " + str(velocity) + " not found at grid links.")
+
+        self._grid.add_zeros('advection_flux', at = 'link')
+
+        self._parallel_links = self.set_parallel_links_at_link(self._grid)
+        self._upwind_links = self.find_upwind_link_at_link(self._grid, self._vel)
+
+        self._grid.add_zeros('flux', at = 'link')
+        self._grid.add_zeros('flux_div', at = 'node')
+
+    def set_parallel_links_at_link(self, grid: RasterModelGrid) -> np.ndarray:
+        """Map each link to its neighboring links that are parallel and directly adjacent."""
+        parallel_links = -np.ones((grid.number_of_links, 2), dtype=int)
+
+        parallel_links[grid.vertical_links, 0] = (
+            grid.links_at_node[grid.node_at_link_tail[grid.vertical_links], 3]
+        )
+        parallel_links[grid.vertical_links, 1] = (
+            grid.links_at_node[grid.node_at_link_head[grid.vertical_links], 1]
+        )
+        parallel_links[grid.horizontal_links, 0] = (
+            grid.links_at_node[grid.node_at_link_tail[grid.horizontal_links], 2]
+        )
+        parallel_links[grid.horizontal_links, 1] = (
+            grid.links_at_node[grid.node_at_link_head[grid.horizontal_links], 0]
+        )
+
+        boundary_tail = grid.status_at_node[grid.node_at_link_tail] != grid.BC_NODE_IS_CORE
+        parallel_links[boundary_tail, 0] = -1
+        boundary_head = grid.status_at_node[grid.node_at_link_head] != grid.BC_NODE_IS_CORE
+        parallel_links[boundary_head, 1] = -1
+
+        return parallel_links
+
+    def find_upwind_link_at_link(self, grid: RasterModelGrid, velocity: str) -> np.ndarray:
+        """Identify extension of each link in the upwind direction."""
+        upwind_links = np.zeros(grid.number_of_links, dtype = int)
+
+        upwind_links[:] = np.where(
+            grid.at_link[velocity][:] > 0,
+            self._parallel_links[:, 0],
+            self._parallel_links[:, 1]
+        )
+
+        return upwind_links
+
+    def calc_courant(self, grid: RasterModelGrid, velocity: str, dt: float) -> np.ndarray:
+        """Return the local Courant number at each link."""
+
+        courant = np.zeros(grid.number_of_links)
+
+        courant[grid.horizontal_links] = (
+            grid.at_link[velocity][grid.horizontal_links] * dt 
+            / grid.spacing[0]
+        )
+        
+        courant[grid.vertical_links] = (
+            grid.at_link[velocity][grid.vertical_links] * dt 
+            / grid.spacing[1]
+        )
+
+        return courant
+
+    def map_value_to_links_linear_upwind(self, grid: RasterModelGrid, field: str, velocity: str) -> np.ndarray:
+        """Map node values to links with a linear upwind method."""
+        value_at_links = np.zeros(grid.number_of_links)
+        velocity_is_positive = grid.at_link[velocity] > 0
+
+        value_at_links[velocity_is_positive] = (
+            grid.at_node[field][grid.node_at_link_tail[velocity_is_positive]]
+        )
+
+        value_at_links[~velocity_is_positive] = (
+            grid.at_node[field][grid.node_at_link_head[~velocity_is_positive]]
+        )
+
+        return value_at_links
+
+    def map_value_to_links_lax_wendroff(self, grid: RasterModelGrid, field: str, velocity: str, dt: float) -> np.ndarray:
+        """Map node values to links with a Lax-Wendroff method."""
+        courant = self.calc_courant(grid, velocity, dt)
+
+        value_at_links = 0.5 * (
+            (1 + courant) * grid.at_node[field][grid.node_at_link_tail] +
+            (1 - courant) * grid.at_node[field][grid.node_at_link_head]
+        )
+
+        return value_at_links
+
+    def calc_upwind_to_local_grad_ratio(self, grid: RasterModelGrid, field: str, velocity: str, dt: float) -> np.ndarray:
+        """Return the ratio of upwind to local gradients."""
+        courant = self.calc_courant(grid, velocity, dt)
+        heads = grid.node_at_link_head
+        tails = grid.node_at_link_tail
+        local_diff = grid.at_node[field][heads] - grid.at_node[field][tails]
+
+        ratio = -np.ones(grid.number_of_links)
+
+        well_defined = (self._upwind_links != -1) & (local_diff != 0.0)
+
+        ratio[well_defined] = (
+            local_diff[self._upwind_links[well_defined]] /
+            local_diff[well_defined]
+        )
+
+        return ratio
+
+    def calc_flux_limiter(self, ratio: np.ndarray) -> np.ndarray:
+        """Return the value of the Van Leer flux limiter at links."""
+        flux_limiter = (ratio + np.abs(ratio)) / (1.0 + np.abs(ratio))
+        return flux_limiter
+
+    def calc_rate_of_change(self, grid: RasterModelGrid, field: str, velocity: str, dt: float):
+        """Return the rate of change due to advection at grid nodes."""
+        low_value_at_links = self.map_value_to_links_linear_upwind(grid, field, velocity)
+        high_value_at_links = self.map_value_to_links_lax_wendroff(grid, field, velocity, dt)
+        ratio = self.calc_upwind_to_local_grad_ratio(grid, field, velocity, dt)
+        flux_limiter = self.calc_flux_limiter(ratio)
+        
+        field_at_links = flux_limiter * high_value_at_links + (1.0 - flux_limiter) * low_value_at_links
+        
+        grid.at_link['flux'][grid.active_links] = (
+            grid.at_link[velocity][grid.active_links] * field_at_links[grid.active_links]
+        )
+        
+        grid.at_node['flux_div'][:] = -grid.calc_flux_div_at_node('flux')
+
+        return grid.at_node['flux_div']
+
+    def update(self, dt: float):
+        """Run one forward time step of (dt) seconds."""
+        rate_of_change = self.calc_rate_of_change(self._grid, self._field, self._vel, dt)
+        self._grid.at_node[self._field][:] += rate_of_change[:] * dt
+        self.time_elapsed += dt
--- basis/test/test_tvd_advection.py
+++ basis/test/test_tvd_advection.py
@@ -1,6 +1,78 @@
 import pytest
 import numpy as np
-from numpy.testing import assert_array_almost_equal, assert_approx_equal
+from landlab import RasterModelGrid
+from numpy.testing import assert_array_equal, assert_approx_equal
 
-from src.tvd_advect import AdvectTVD
+from basis.src.tvd_advection import AdvectTVD
 
+@pytest.fixture
+def tvd():
+    grid = RasterModelGrid((5, 5), 10.)
+    grid.add_ones('test', at = 'node')
+    grid.at_node['test'][12] = 10
+
+    grid.add_zeros('velocity', at = 'link')
+    grid.at_link['velocity'][grid.active_links] = 5.0
+
+    tvd = AdvectTVD(grid, 'test', 'velocity')
+    return tvd
+
+def test_set_parallel_links(tvd):
+    assert_array_equal(tvd._parallel_links[15], [6, 24])
+    assert_array_equal(tvd._parallel_links[20], [19, 21])
+
+def test_find_upwind_links(tvd):
+    assert_array_equal(tvd._upwind_links[15], [6])
+    assert_array_equal(tvd._upwind_links[20], [19])
+
+def test_calc_courant(tvd):
+    courant = tvd.calc_courant(tvd._grid, tvd._vel, 100.0)
+    assert_array_equal(courant[15], [50.0])
+
+def test_map_value_linear_upwind(tvd):
+    value_at_links = tvd.map_value_to_links_linear_upwind(tvd._grid, tvd._field, tvd._vel)
+    assert_array_equal(value_at_links[15], [1])
+    assert_array_equal(value_at_links[24], [10])
+    assert_array_equal(value_at_links[19], [1])
+    assert_array_equal(value_at_links[20], [10])
+
+def test_map_value_lax_wendroff(tvd):
+    value_at_links = tvd.map_value_to_links_lax_wendroff(tvd._grid, tvd._field, tvd._vel, 1.0)
+    assert_array_equal(value_at_links[15], [3.25])
+    assert_array_equal(value_at_links[24], [7.75])
+    assert_array_equal(value_at_links[19], [3.25])
+    assert_array_equal(value_at_links[20], [7.75])
+
+def test_calc_grad_ratio(tvd):
+    ratio = tvd.calc_upwind_to_local_grad_ratio(tvd._grid, tvd._field, tvd._vel, 1.0)
+    assert_array_equal(ratio[15], [0.0])
+    assert_array_equal(ratio[24], [-1])
+    assert_array_equal(ratio[19], [0.0])
+    assert_array_equal(ratio[20], [-1])
+
+def test_calc_flux_limiter(tvd):
+    ratio = tvd.calc_upwind_to_local_grad_ratio(tvd._grid, tvd._field, tvd._vel, 1.0)
+    flux_limiter = tvd.calc_flux_limiter(ratio)
+    assert_array_equal(flux_limiter[15], [0.0])
+    assert_array_equal(flux_limiter[24], [0.0])
+    assert_array_equal(flux_limiter[19], [0.0])
+    assert_array_equal(flux_limiter[20], [0.0])
+
+def test_calc_rate_of_change(tvd):
+    tvd.calc_rate_of_change(tvd._grid, tvd._field, tvd._vel, 1.0)
+
+    assert_array_equal(tvd._grid.at_link['flux'][15], [5.0])
+    assert_array_equal(tvd._grid.at_link['flux'][24], [50.0])
+    assert_array_equal(tvd._grid.at_link['flux'][19], [5.0])
+    assert_array_equal(tvd._grid.at_link['flux'][20], [50.0])
+
+    assert_array_equal(tvd._grid.at_node['flux_div'][12], [-9.0])
+    assert_array_equal(tvd._grid.at_node['flux_div'][13], [4.5])
+    assert_array_equal(tvd._grid.at_node['flux_div'][17], [4.5])
+
+def test_update(tvd):
+    tvd.update(1.0)
+    assert tvd.time_elapsed == 1.0
+    assert_array_equal(tvd._grid.at_node['test'][12], 1.0)
+    assert_array_equal(tvd._grid.at_node['test'][13], 5.5)
+    assert_array_equal(tvd._grid.at_node['test'][17], 5.5)
