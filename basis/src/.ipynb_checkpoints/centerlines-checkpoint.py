81953446c06c47595c0dd382061566c5e926f1cf
Ethan Pierce <ethan.pierce@colorado.edu>
2023-05-26 16:12:40 -0600

    Create prototype notebook for centerline figure

--- basis/src/.ipynb_checkpoints/centerlines-checkpoint.py
+++ basis/src/.ipynb_checkpoints/centerlines-checkpoint.py
@@ -1,10 +1,10 @@
 """Utility to identify centerlines of polygons."""
 import numpy as np
-from scipy.ndimage import label, generate_binary_structure, binary_erosion
+from scipy.ndimage import label, generate_binary_structure
 
 def identify_centerline(masked_array: np.ndarray) -> np.ndarray:
     """Identify the centerline of shapes in a binary array."""
-    centerline = np.zeros_like(np.logical_not(masked_array))
+    centerline = np.zeros_like(masked_array)
 
     labeled_array, n_labels = label(masked_array)
     structure = generate_binary_structure(2, 1)
@@ -12,9 +12,9 @@ def identify_centerline(masked_array: np.ndarray) -> np.ndarray:
     for lab in range(1, n_labels + 1):
         label_mask = labeled_array == lab
         dilated_mask = np.logical_and(label_mask, np.logical_not(centerline))
-        eroded_mask = np.logical_and(label_mask, centerline)
 
-        eroded_mask = binary_erosion(eroded_mask, structure)
+        eroded_mask = np.logical_and(label_mask, centerline)
+        eroded_mask = np.logical_and(eroded_mask, structure)
 
         centerline[dilated_mask] = True
         centerline[eroded_mask] = False
